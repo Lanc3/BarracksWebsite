2a06e5260c5acc088781f19b5035324c98bef5e8
Aaron Keating <aaron.keating.lanc3@gmail.com>
2024-01-10 20:09:03 +0000

    header font size

--- web/src/components/LandingSection/LandingSection.jsx
+++ web/src/components/LandingSection/LandingSection.jsx
@@ -1,89 +1,86 @@
 import { Link, routes } from '@redwoodjs/router'
 
-import Illustration from '../../../public/glow-bottom.svg'
 import buildingImage from '../../../public/barracks-carlow_0688.jpg'
+import Illustration from '../../../public/glow-bottom.svg'
 import Particles from '../Particles/Particles'
 const LandingSection = () => {
   return (
     <div>
       <div className="">
+        <img
+          src={buildingImage}
+          className=" absolute z-0 mx-auto max-h-[621px] min-h-[621px]  min-w-full"
+          alt="Hero Illustration"
+        />
+      </div>
 
-      <img
-        src={buildingImage}
-        className=" z-0 absolute min-h-[621px] mx-auto max-h-[621px]  min-w-full"
-        alt="Hero Illustration"
-      />
-</div>
+      <div className="sm:max-w-auto relative mx-auto -mt-20 min-h-[621px] max-w-6xl break-words  px-4 sm:px-2">
+        <Particles className="absolute inset-0 z-10 w-screen" />
 
-<div className="sm:max-w-auto relative -mt-20 mx-auto min-h-[621px] max-w-6xl break-words  px-4 sm:px-2">
-  <Particles className="absolute inset-0 z-10 w-screen" />
-
-  <div
-    className=" absolute inset-0 z-20 -mx-28 overflow-hidden rounded-b-[3rem] "
-    aria-hidden="true"
-  >
-
-    <div className="absolute -bottom-0 left-1/2 -z-10 -translate-x-1/2 translate-y-1/4">
-      <img
-        src={Illustration}
-        className="max-w-none "
-        alt="Hero Illustration"
-      />
-    </div>
-    <div className="z-30 pb-16 pt-32 md:pb-16 ">
-      {/* Hero content */}
-      <div className="mx-auto max-w-3xl text-center">
-        <div className="mb-6" data-aos="fade-down">
-        </div>
-        <h1
-          className="h1 break-words bg-gradient-to-r from-purple-500 to-purple-200 bg-clip-text px-24 pb-4 text-transparent sm:px-24"
-          data-aos="fade-down"
-        >
-          Welcome To <br/>The Barracks
-        </h1>
-        <p
-          className="mb-8 break-words px-32 text-lg text-slate-300 sm:px-32"
-          data-aos="fade-down"
-          data-aos-delay="200"
-        >
-          Step into The Barracks, where every drink tells a story and every
-          moment feels like a celebration – your new haven for camaraderie,
-          crafted cocktails, and unforgettable memories.
-        </p>
         <div
-          className="mx-auto max-w-xs space-y-4 sm:inline-flex sm:max-w-none sm:justify-center sm:space-x-4 sm:space-y-0"
-          data-aos="fade-down"
-          data-aos-delay="400"
+          className=" absolute inset-0 z-20 -mx-28 overflow-hidden rounded-b-[3rem] "
+          aria-hidden="true"
         >
-          <div>
-            <Link
-              className="btn group w-full bg-gradient-to-r from-white/80 via-white to-white/80 text-slate-900 transition duration-150 ease-in-out hover:bg-white"
-              to={routes.contactus()}
-            >
-              Book With Us{' '}
-              <span className="ml-1 tracking-normal text-purple-500 transition-transform duration-150 ease-in-out group-hover:translate-x-0.5">
-                -&gt;
-              </span>
-            </Link>
+          <div className="absolute -bottom-0 left-1/2 -z-10 -translate-x-1/2 translate-y-1/4">
+            <img
+              src={Illustration}
+              className="max-w-none "
+              alt="Hero Illustration"
+            />
           </div>
-          <div>
-            <Link
-              className="btn w-full bg-slate-900 bg-opacity-25 text-slate-200 transition duration-150 ease-in-out hover:bg-opacity-30 hover:text-white"
-              to={routes.events()}
-            >
-              <span>See Our Events</span>
-              <span className="ml-1 tracking-normal text-purple-500 transition-transform duration-150 ease-in-out group-hover:translate-x-0.5">
-                -&gt;
-              </span>
-            </Link>
+          <div className="z-30 pb-16 pt-32 md:pb-16 ">
+            {/* Hero content */}
+            <div className="mx-auto max-w-3xl text-center">
+              <div className="mb-6" data-aos="fade-down"></div>
+              <h1
+                className="h1 break-words bg-gradient-to-r from-purple-500 to-purple-200 bg-clip-text px-24 pb-4 text-transparent sm:px-24"
+                data-aos="fade-down"
+              >
+                Welcome to <br />
+                The Barracks
+              </h1>
+              <p
+                className="mb-8 break-words px-32 text-lg text-slate-300 sm:px-32"
+                data-aos="fade-down"
+                data-aos-delay="200"
+              >
+                Step into The Barracks, where every drink tells a story and
+                every moment feels like a celebration – your new haven for
+                camaraderie, crafted cocktails, and unforgettable memories.
+              </p>
+              <div
+                className="mx-auto max-w-xs space-y-4 sm:inline-flex sm:max-w-none sm:justify-center sm:space-x-4 sm:space-y-0"
+                data-aos="fade-down"
+                data-aos-delay="400"
+              >
+                <div>
+                  <Link
+                    className="btn group w-full bg-gradient-to-r from-white/80 via-white to-white/80 text-slate-900 transition duration-150 ease-in-out hover:bg-white"
+                    to={routes.contactus()}
+                  >
+                    Book With Us{' '}
+                    <span className="ml-1 tracking-normal text-purple-500 transition-transform duration-150 ease-in-out group-hover:translate-x-0.5">
+                      -&gt;
+                    </span>
+                  </Link>
+                </div>
+                <div>
+                  <Link
+                    className="btn w-full bg-slate-900 bg-opacity-25 text-slate-200 transition duration-150 ease-in-out hover:bg-opacity-30 hover:text-white"
+                    to={routes.events()}
+                  >
+                    <span>See Our Events</span>
+                    <span className="ml-1 tracking-normal text-purple-500 transition-transform duration-150 ease-in-out group-hover:translate-x-0.5">
+                      -&gt;
+                    </span>
+                  </Link>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-</div>
-    </div>
-
   )
 }
 
--- web/src/pages/HomePage/HomePage.jsx
+++ web/src/pages/HomePage/HomePage.jsx
@@ -2,49 +2,65 @@ import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 
 import BackgroundGallary from 'src/components/BackgroundGallary/BackgroundGallary'
+import BookingPriceSection from 'src/components/BookingPriceSection/BookingPriceSection'
+import Carousel from 'src/components/Carousel/Carousel'
 import Clients from 'src/components/Clients/Clients'
 import ContactSection from 'src/components/ContactSection/ContactSection'
 import LandingSection from 'src/components/LandingSection/LandingSection'
-import SliderGallary from 'src/components/SliderGallary/SliderGallary'
-import BookingPriceSection from 'src/components/BookingPriceSection/BookingPriceSection'
-import TeamHero from 'src/components/TeamHero/TeamHero'
 import OpeningTimetable from 'src/components/OpeningTimetable/OpeningTimetable'
-import Carousel from 'src/components/Carousel/Carousel'
-import gal1 from "../../../public/GAL1.webp"
-import gal2 from "../../../public/GAL2.webp"
-import gal3 from "../../../public/GAL3.webp"
-import gal4 from "../../../public/GAL4.webp"
-import gal5 from "../../../public/GAL5.webp"
-import gal6 from "../../../public/GAL6.webp"
-import gal7 from "../../../public/GAL7.webp"
-import gal8 from "../../../public/GAL8.webp"
-import gal9 from "../../../public/GAL9.webp"
-import gal10 from "../../../public/GAL10.webp"
-import gal11 from "../../../public/GAL11.webp"
-import gal12 from "../../../public/GAL12.webp"
-import gal13 from "../../../public/GAL13.webp"
+import SliderGallary from 'src/components/SliderGallary/SliderGallary'
+import TeamHero from 'src/components/TeamHero/TeamHero'
+
+import gal1 from '../../../public/GAL1.webp'
+import gal10 from '../../../public/GAL10.webp'
+import gal11 from '../../../public/GAL11.webp'
+import gal12 from '../../../public/GAL12.webp'
+import gal13 from '../../../public/GAL13.webp'
+import gal2 from '../../../public/GAL2.webp'
+import gal3 from '../../../public/GAL3.webp'
+import gal4 from '../../../public/GAL4.webp'
+import gal5 from '../../../public/GAL5.webp'
+import gal6 from '../../../public/GAL6.webp'
+import gal7 from '../../../public/GAL7.webp'
+import gal8 from '../../../public/GAL8.webp'
+import gal9 from '../../../public/GAL9.webp'
 const HomePage = () => {
-  const images = [gal1,gal2,gal3,gal3,gal4,gal5,gal6,gal7,gal8,gal9,,gal10,gal11,gal12,gal13]
+  const images = [
+    gal1,
+    gal2,
+    gal3,
+    gal3,
+    gal4,
+    gal5,
+    gal6,
+    gal7,
+    gal8,
+    gal9,
+    gal10,
+    gal11,
+    gal12,
+    gal13,
+  ]
   return (
     <div className="mb-24 ">
       <LandingSection />
       <Clients />
       <ContactSection />
       <div>
-      <div className="flex w-full justify-center">
+        <div className="flex w-full justify-center">
           <h1
-            className="h2 mt-6 bg-gradient-to-r from-slate-200/60 via-slate-200 to-slate-200/60 bg-clip-text pb-4 text-transparent text-center"
+            className="h2 mt-6 bg-gradient-to-r from-slate-200/60 via-slate-200 to-slate-200/60 bg-clip-text pb-4 text-center text-transparent"
             data-aos="fade-down"
           >
             Atmospheric Nights Out
           </h1>
         </div>
-      <Carousel images={images}  />
-    </div>
+        <Carousel images={images} />
+      </div>
 
       <BookingPriceSection />
       <TeamHero />
-      <OpeningTimetable/>
+      <OpeningTimetable />
       {/* <SliderGallary /> */}
       <MetaTags title="Home" description="Home page" />
     </div>
